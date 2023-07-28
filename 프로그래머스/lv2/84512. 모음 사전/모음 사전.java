c678ba25f18891a3fe74a93c558ee9fad90011d4
JongWoon Moon <74203371+bombo-dev@users.noreply.github.com>
2023-07-28 23:48:05 +0900

    [level 2] Title: 모음 사전, Time: 13.02 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -3,23 +3,21 @@ import java.util.*;
 class Solution {
     private static final char[] CHARS = "AEIOU".toCharArray();
 
-    public int solution(String word) {   
-        return generate("").indexOf(word);
+    public int solution(String word) {
+        List<String> words = new ArrayList<>();
+        generate("", words);
+        return words.indexOf(word);
     }
     
-    private List<String> generate(String word) {
-        
-        List<String> words = new ArrayList<>();
+    private void generate(String word, List<String> words) {
         words.add(word);
         
         if (word.length() == 5) {
-            return words;
+            return;
         }
         
         for (char c : CHARS) {
-            words.addAll(generate(word + c));
+            generate(word + c, words);
         }
-        
-        return words;
     }
 }
