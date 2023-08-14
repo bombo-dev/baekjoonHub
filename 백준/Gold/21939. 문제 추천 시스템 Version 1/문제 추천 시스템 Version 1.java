ab01118d9cfb80a7978dd854f54d83e9145a8e98
JongWoon Moon <74203371+bombo-dev@users.noreply.github.com>
2023-08-14 10:24:40 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 608 ms, Memory: 55396 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
@@ -1,66 +1,103 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        new Solution().solve();
+    }
 
-    public static class Problem implements Comparable<Problem> {
-        int idx;
-        int level;
+    private static class Solution {
+        private int N;
+        private int M;
+        private TreeSet<Question> questions = new TreeSet<>();
+        private Map<Integer, Question> questionMap = new HashMap<>();
+        private StringBuilder sb = new StringBuilder();
 
-        public Problem(int idx, int level) {
-            this.idx = idx;
-            this.level = level;
+        public void solve() throws IOException {
+            var br = new BufferedReader(new InputStreamReader(System.in));
+            N = Integer.parseInt(br.readLine());
+
+            // 기본적으로는 난이도를 기점으로 보는 이진트리
+            // 다만 똑같은 난이도 일 경우 가장 쉬운 건 문제 번호가 작은거, 어려운 건 문제 번호가 높은 것
+            // 양방향 우선순위 큐를 써야함. 트리셋을 써야함.
+
+            for (int i = 0; i < N; i++) {
+                var st = new StringTokenizer(br.readLine());
+                var number = Integer.parseInt(st.nextToken());
+                var difficulty = Integer.parseInt(st.nextToken());
+                questionMap.put(number, new Question(number, difficulty));
+                questions.add(new Question(number, difficulty));
+            }
+            M = Integer.parseInt(br.readLine());
+
+            recommendQuestion(br);
+            System.out.print(sb);
         }
 
-        //난이도 순으로 정렬 -> 문제 번호로 정렬
-        public int compareTo(Problem o) {
-
-            if (level - o.level == 0) {
-                return idx - o.idx;
-            } else {
-                return level - o.level;
+        private void recommendQuestion(BufferedReader br) throws IOException {
+            for (int i = 0; i < M; i++) {
+                var st = new StringTokenizer(br.readLine());
+                var command = st.nextToken();
+                executeCommand(command, st);
             }
         }
 
-        public String toString(){
-            return "idx : "+idx+" level : "+level;
+        private void executeCommand(String command, StringTokenizer st) {
+            switch (command) {
+                case "add":
+                    var number = Integer.parseInt(st.nextToken());
+                    var difficulty = Integer.parseInt(st.nextToken());
+                    questionMap.put(number, new Question(number, difficulty));
+                    questions.add(new Question(number, difficulty));
+                    break;
+                case "recommend":
+                    var recommend = Integer.parseInt(st.nextToken());
+                    recommend(recommend);
+                    break;
+                case "solved":
+                    var questionNumber = Integer.parseInt(st.nextToken());
+                    questions.remove(questionMap.get(questionNumber));
+                    questionMap.remove(questionNumber);
+            }
+        }
+
+        private void recommend(int recommend) {
+            if (recommend == 1) {
+                sb.append(questions.last().number).append("\n");
+            } else {
+                sb.append(questions.first().number).append("\n");
+            }
+        }
+
+        private void clearAndFill(PriorityQueue<Question> pq) {
+            var recommendQuestion = pq.poll();
+            sb.append(recommendQuestion.number).append("\n");
+
+            while (!pq.isEmpty()) {
+                questions.add(pq.poll());
+            }
         }
     }
 
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+    private static class Question implements Comparable<Question> {
+        int number;
+        int difficulty;
 
-        int n = sc.nextInt();
-
-        TreeSet<Problem> ts = new TreeSet<>();
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            int nowidx = sc.nextInt();
-            int nowlevel = sc.nextInt();
-            ts.add(new Problem(nowidx, nowlevel));
-            map.put(nowidx,nowlevel);
+        public Question(int number, int difficulty) {
+            this.number = number;
+            this.difficulty = difficulty;
         }
-        int m = sc.nextInt();
-        for (int i = 0; i < m; i++) {
-            String command = sc.next(   );
-            if (command.equals("add")) {
-                int nowidx = sc.nextInt();
-                int nowlevel = sc.nextInt();
-                ts.add(new Problem(nowidx, nowlevel));
-                map.put(nowidx,nowlevel);
-            } else {
-                if (command.equals("recommend")) {
-                    if (sc.nextInt() == 1) {
-                        System.out.println(ts.last().idx);
-                    } else {
-                        System.out.println(ts.first().idx);
-                    }
-                } else {
-                    int nowidx = sc.nextInt();
-                    ts.remove(new Problem(nowidx,map.get(nowidx)));
-                    map.remove(nowidx);
-                }
+
+        @Override
+        public int compareTo(Question q) {
+            if (this.difficulty == q.difficulty) {
+                return this.number - q.number;
             }
+
+            return this.difficulty - q.difficulty;
         }
     }
 }
