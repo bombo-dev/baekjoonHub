566ea1ba2db82debdb4c7a179e0f75bb248235a7
JongWoon Moon <dkflfkdtm2@gmail.com>
2023-12-23 22:43:37 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1276 ms, Memory: 314028 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,96 +1,104 @@
-import java.io.*;
-import java.util.*;
-
-class Wall {
-    int x;
-    int y;
-    int preWall;
-    int time;
-
-    public Wall(int x, int y, int preWall, int time) {
-        this.x = x;
-        this.y = y;
-        this.preWall = preWall;
-        this.time = time;
-    }
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
-    static int K;
-    static int[][] graph;
-    static boolean[][][] visited;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-
-    static int result = Integer.MAX_VALUE;
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        graph = new int[N][M];
-        visited = new boolean[K + 1][N][M]; // 0은 벽을 안 부심
-
-        for(int i = 0; i < N; i++) {
-            String value = br.readLine();
-            for(int j = 0; j < M; j++) {
-                graph[i][j] = Character.getNumericValue(value.charAt(j));
-            }
-        }
-        BFS();
-
-        if(result == Integer.MAX_VALUE) {
-            System.out.print(-1);
-        } else {
-            System.out.print(result);
-        }
+        new Solution().solve();
     }
 
-    public static void BFS() {
-        Queue<Wall> q = new LinkedList<>();
-        q.offer(new Wall(0, 0, 0, 1));
-        visited[0][0][0] = true;
-        while(!q.isEmpty()) {
-            Wall w = q.poll();
-            int px = w.x;
-            int py = w.y;
-            int preWall = w.preWall;
-            int time = w.time;
+    private static class Solution {
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private int max = 0;
 
-            if(px == N - 1 && py == M - 1) {
-                result = Math.min(result, time);
+        private int N, M, K;
+
+        private int[] dx = new int[]{-1, 0, 1, 0};
+        private int[] dy = new int[]{0, 1, 0, -1};
+        private int[][] boards;
+        private boolean[][][] visited;
+
+        public void solve() throws IOException {
+            StringTokenizer st = getStringTokenizer();
+            N = stoi(st.nextToken());
+            M = stoi(st.nextToken());
+            K = stoi(st.nextToken());
+
+            boards = new int[N + 1][M + 1];
+            visited = new boolean[K + 1][N + 1][M + 1];
+
+            for (int i = 1; i <= N; i++) {
+                String value = br.readLine();
+                for (int j = 0; j < M; j++) {
+                    boards[i][j + 1] = Character.getNumericValue(value.charAt(j));
+                }
             }
 
-            for(int i = 0; i < 4; i++) {
-                int nx = px + dx[i];
-                int ny = py + dy[i];
+            bfs();
 
-                if(nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                    if(preWall < K) {
-                        if(graph[nx][ny] == 1) {
-                            if(!visited[preWall + 1][nx][ny]) {
-                                q.offer(new Wall(nx, ny, preWall + 1, time + 1));
-                                visited[preWall + 1][nx][ny] = true;
-                            }
-                        } else {
-                            if(!visited[preWall][nx][ny]) {
-                                q.offer(new Wall(nx, ny, preWall, time + 1));
-                                visited[preWall][nx][ny] = true;
-                            }
-                        }
+            if (max == 0) {
+                System.out.print(-1);
+            } else {
+                System.out.print(max);
+            }
+        }
+
+        private void bfs() {
+            visited[0][1][1] = true;
+
+            Deque<int[]> dq = new ArrayDeque<>();
+            dq.offerLast(new int[]{1, 1, 0, 1});
+
+            while (!dq.isEmpty()) {
+                int[] now = dq.pollFirst();
+                int x = now[0];
+                int y = now[1];
+                int visit = now[2];
+                int count = now[3];
+
+                if (x == N && y == M) {
+                    max = count;
+                    return;
+                }
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+
+                    if (nx < 1 || nx > N || ny < 1 || ny > M) {
+                        continue;
+                    }
+
+                    if (visited[visit][nx][ny]) {
+                        continue;
+                    }
+
+                    if (boards[nx][ny] == 0) {
+                        visited[visit][nx][ny] = true;
+                        dq.offerLast(new int[]{nx, ny, visit, count + 1});
                     } else {
-                        if(graph[nx][ny] == 0 && !visited[preWall][nx][ny]) {
-                            q.offer(new Wall(nx, ny, preWall, time + 1));
-                            visited[preWall][nx][ny] = true;
+                        if (visit < K && !visited[visit + 1][nx][ny]) {
+                            visited[visit + 1][nx][ny] = true;
+                            dq.offerLast(new int[]{nx, ny, visit + 1, count + 1});
                         }
                     }
                 }
             }
+        }
+
+        private StringTokenizer getStringTokenizer(String delim) throws IOException {
+            return new StringTokenizer(br.readLine(), delim);
+        }
+
+        private StringTokenizer getStringTokenizer() throws IOException {
+            return new StringTokenizer(br.readLine());
+        }
+
+        private int stoi(String number) {
+            return Integer.parseInt(number);
         }
     }
 }
