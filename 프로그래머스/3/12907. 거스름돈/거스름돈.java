69d210f1702aa9ec9efff4ccae1d4270ae1717d1
JongWoon Moon <dkflfkdtm2@gmail.com>
2024-01-18 17:50:24 +0900

    [level 3] Title: 거스름돈, Time: 14.91 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,23 +1,17 @@
 import java.util.*;
 
 class Solution {
-    // money의 배열의 길이 100
-    // 시작 금액의 최대 10만
-    // 개수는 1,000,000,007로 나눈 나머지 
-    // 모듈러 연산은 계산 결과에 연산을 미치지 않으므로 항상 결과에 나머지를 사용하기.
-    private static final int MODULAR = 1000000007;
-    
     public int solution(int n, int[] money) {
         int[] dp = new int[n + 1];
-        dp[0] = 1; // 금액이 0인 경우의 초기값 설정
-
-        for (int coin : money) {
-            for (int i = coin; i <= n; i++) {
-                dp[i] += dp[i - coin];
-                dp[i] %= MODULAR;
+        dp[0] = 1;
+        
+        for (int value : money) {
+            for (int i = value; i <= n; i++) {
+                dp[i] = dp[i] + dp[i - value];
             }
         }
-
-        return dp[n];
+        
+        int answer = dp[n];
+        return answer;
     }
 }
